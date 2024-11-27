d40c5591772646b5980b6335d6ee4d758d3d1182
Sparsh-I <sparsh.poonacha@gmail.com>
2024-11-27 15:57:56 -0800

    Initial commit

---
+++ Snow Project/Assets/Scripts/GroundTileManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GroundTileManager : MonoBehaviour
+{
+    public GameObject groundTilePrefab; // Prefab for ground tiles
+    public Transform player; // Reference to the player
+    public float tileLength = 10f; // Length of a single tile
+    public int initialTileCount = 5; // How many tiles to start with
+    public float spawnThreshold = 10f; // Distance ahead of the player to spawn new tiles
+
+    private Queue<GameObject> activeTiles = new Queue<GameObject>();
+    private float spawnZ = 0f; // Z position for the next tile
+    private float lastPlayerZ; // To track player's last position for despawning
+
+    void Start()
+    {
+        // Spawn initial tiles
+        for (int i = 0; i < initialTileCount; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    void Update()
+    {
+        // If the player has passed the first tile in the queue by the tile length
+        if (player.position.x > activeTiles.Peek().transform.position.x + tileLength)
+        {
+            // Remove the oldest tile
+            DespawnTile();
+
+            // Spawn a new tile at the end of the queue (right after the last tile)
+            SpawnTile();
+        }
+    }
+
+    void SpawnTile()
+    {
+        // Instantiate a new tile at the spawn position
+        GameObject newTile = Instantiate(groundTilePrefab, new Vector3(spawnZ, 0, 0), Quaternion.identity);
+        activeTiles.Enqueue(newTile);
+        spawnZ += tileLength; // Move spawn position forward
+    }
+
+    void DespawnTile()
+    {
+        // Remove the oldest tile
+        GameObject oldTile = activeTiles.Dequeue();
+        Destroy(oldTile);
+    }
+}
---
+++ Snow Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float rotationSpeed;
+
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private Transform groundCheck; // Assign a small empty GameObject below the player
+    [SerializeField] private float groundCheckRadius = 0.5f;
+    [SerializeField] private LayerMask groundLayer; // Assign "Ground" layer to ground objects
+    
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(playerSpeed, rb.velocity.y);
+        isGrounded = IsGrounded();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+}
